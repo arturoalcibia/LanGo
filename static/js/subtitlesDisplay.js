1c08e9873848ef8ecc25a94cfe52529dc8aea56f
arturoalcibia <arturoalcibia@hotmail.com>
2021-10-02 12:13:53 -0700

    Refactor subtitles display. - Add slider on before and after subtitles

--- static/js/subtitlesDisplay.js
+++ static/js/subtitlesDisplay.js
@@ -4,68 +4,159 @@ var subtitlesDiv = document.getElementsByClassName('sub');
 // Get settings
 var beforeSettingsInput = document.getElementById("beforeSettingsInput");
 beforeSettingsInput.value = 1
+beforeSettingsInput.addEventListener('input', refreshSubtitles);
 var afterSettingsInput  = document.getElementById("afterSettingsInput");
 afterSettingsInput.value = 1
+afterSettingsInput.addEventListener('input', refreshSubtitles);
+
+var loopUntilCheckBox = document.getElementById('loopCheckBox');
+loopUntilCheckBox.checked = true;
 
 // Add all eventListener to input
 for (let i = 0; i < subtitlesDiv.length; i++) {
   subtitleDiv = subtitlesDiv[i];
   inputChildren = subtitleDiv.getElementsByClassName('inputSub');
+}
 
-  /*
-  //todo! this doesnt werk
-  for (let j = 0; j < inputChildren.length; j++) {
-     inputChildren[j].addEventListener('input', logValue);
+function __clearVisible(subToSkip){
+  // Remove any visible subtitles.
+  var visibleSubs = document.getElementsByClassName('visible');
+  for (let i = 0; i < visibleSubs.length; i++){
+
+    visibleSub = visibleSubs[i];
+
+    // Omit the current sub.
+    if (visibleSub === subToSkip)
+      continue
+
+    visibleSub.classList.remove('visible');
   }
-   */
+}
+
+function __isTimeBetweenRange(inCurrentTime, inDiv){
+  // Return true if div dataset start and end are between the provided time.
+  return (inCurrentTime >= inDiv.dataset.start && inCurrentTime <= inDiv.dataset.end)
+}
+
+function __setVisibleNeighbours(inRange, inCurrentSub, inIsBefore){
+  tempSub = inCurrentSub;
+
+  // Display subtitles before/after.
+  for (let i = 1; i < inRange; i++){
+
+    if (inIsBefore)
+      newSub = tempSub.previousElementSibling;
+    else
+      newSub = tempSub.nextElementSibling;
+
+    if (newSub === null)
+      return;
+
+    newSub.classList.add('visible');
+    tempSub = newSub;
+  }
 }
 
 function displaySubtitles(){
+  // Called on an interval.
+  // Function to display subtitles based on the current settings.
   currentTime = player.getCurrentTime();
-  var indicesToVisible   = []
+  var currentIndex   = null;
 
   for (let i = 0; i < subtitlesDiv.length; i++) {
 
     subtitleDiv = subtitlesDiv[i]
 
-    //todo! Maybe check if class is already on the list?
-    if (currentTime >= subtitleDiv.dataset.start && currentTime <= subtitleDiv.dataset.end) {
-      indicesToVisible.push(i);
+    if (__isTimeBetweenRange(currentTime, subtitleDiv))
+    {
+      currentIndex = i;
+      break;
+    }
+  }
 
-      for (let j = 1; j < (parseInt(beforeSettingsInput.value) + 1); j++) {
+  if (currentIndex !== null){
 
-        newIndexBefore = i - j;
+    // Check if subtitles need to change or looped over.
+    var changeSubsBool = true;
+    var previousCurrentSubtitle = document.getElementById('current');
+    var visibleSubs = document.getElementsByClassName('visible');
 
-        if (newIndexBefore < 1)
-          continue
+    if (loopUntilCheckBox.checked && previousCurrentSubtitle !== null){
+      changeSubsBool = false;
 
-        indicesToVisible.push(newIndexBefore);
+      // Set video current time.
+      // if settings are only one visible video.
+      if (visibleSubs.length === 1){
+        startTime = visibleSubs[0].dataset.start;
+        endTime = visibleSubs[0].dataset.end;
       }
 
-      for (let k = 0; k < parseInt(afterSettingsInput.value) + 1; k++) {
-
-        newIndexAfter = k + i;
-
-        if (newIndexAfter >= subtitlesDiv.length )
-          continue
-
-        indicesToVisible.push(newIndexAfter);
+      else {
+        startTime = visibleSubs[0].dataset.start;
+        endTime = visibleSubs[visibleSubs.length - 1].dataset.end;
       }
+
+      if (currentTime > endTime)
+        player.seekTo(startTime - 0.100);
 
     }
 
+    if (!(changeSubsBool))
+      return
+
+    // Remove any visible subtitles
+    for (let i = 0; i < visibleSubs.length; i++)
+      visibleSubs[i].classList.remove('visible');
+
+    currentSubtitle = subtitlesDiv[currentIndex];
+
+    // If any current sub already, unset them to set the new one.
+    if (currentSubtitle !== previousCurrentSubtitle){
+
+      if (previousCurrentSubtitle !== null)
+        previousCurrentSubtitle.id = '';
+
+      currentSubtitle = subtitlesDiv[currentIndex]
+      currentSubtitle.classList.add('visible');
+      currentSubtitle.id = 'current';
+    }
+
+    // Display subtitles before.
+    __setVisibleNeighbours(
+        (parseInt(beforeSettingsInput.value) + 1),
+        currentSubtitle,
+        true);
+
+    // Display subtitles after.
+    __setVisibleNeighbours(
+        parseInt(afterSettingsInput.value) + 1,
+        currentSubtitle,
+        false);
   }
 
-  if (indicesToVisible.length !== 0){
+}
 
-  var visibleSubs = document.getElementsByClassName('visible');
+function refreshSubtitles(){
 
-  for (let i = 0; i < visibleSubs.length; i++)
-    visibleSubs[i].classList.remove('visible');
+  var currentSub = document.getElementById('current');
 
-  for (let i = 0; i < indicesToVisible.length; i++)
-    subtitlesDiv[indicesToVisible[i]].classList.add('visible');
+  // No subtitles to display.
+  if (currentSub === null)
+    return
 
-  }
+  // Remove any visible subtitles.
+  __clearVisible(currentSub);
+
+  // Display subtitles before.
+  __setVisibleNeighbours(
+      (parseInt(beforeSettingsInput.value) + 1),
+      currentSubtitle,
+      true);
+
+  // Display subtitles after.
+  __setVisibleNeighbours(
+      parseInt(afterSettingsInput.value) + 1,
+      currentSubtitle,
+      false);
 
 }
